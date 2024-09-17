93a8db84b752640a3543fa896ce0dca231080d84
Yitzchak Novick <yitzchak.novick@touro.edu>
2024-09-17 18:37:32 -0400

    rename msg var

--- src/yn/Program.java
+++ src/yn/Program.java
@@ -3,8 +3,8 @@ package yn;
 public class Program {
 
 	public static void main(String[] args) {
-		String msg = MessageGenerator.getMessage();
-		System.out.println(msg);
+		String message = MessageGenerator.getMessage();
+		System.out.println(message);
 	}
 
 }
